f96776e24eba3872b10e6ae98b2426d2325c20a0
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-06-18 19:17:17 +0530

    Update Sample2.java

--- Sample2.java
+++ Sample2.java
@@ -2,7 +2,7 @@ class Sample2{
     public void display(){
         System.out.println("Welcome");
     }
-    public static public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Welcome");
     }
 }
