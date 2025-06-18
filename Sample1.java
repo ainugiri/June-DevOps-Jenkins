5d8e384d0ba5bb8dd4196393a812ccab5947ef69
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-06-18 18:26:23 +0530

    merged

--- Sample1.java
+++ Sample1.java
@@ -2,20 +2,13 @@ class Sample1{
     public void display(){
         System.out.println("Welcome");
     }
-<<<<<<< HEAD
-    public static public static void main(String[] args) {
-        System.out.println("Welcome");
-    }
-}
-=======
 
     public static void main(String[] args) 
     {
 
-Sample1 A = new Sample1(); 
+    Sample1 A = new Sample1(); 
 
         A.display(); 
         
     }
 }
->>>>>>> e60069aec2f5c1f7c44c0e37f654d231bb9f022a
