2253a22cd02e461e5958bc4429b6b0ff72d32d4f
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-06-19 19:17:48 +0530

    Update Sample1.java

--- Sample1.java
+++ Sample1.java
@@ -1,24 +1,11 @@
 class Sample1{
     public void display(){
         System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
         System.out.println("Giri Prasad P");
     }
-
     public static void main(String[] args) 
     {
-
     Sample1 A = new Sample1(); 
-
         A.display(); 
-        
     }
 }
