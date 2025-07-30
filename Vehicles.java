cb90cd0ffe73843093ae1eab93479e910b14d674
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-07-30 11:00:17 +0530

    Update Vehicles.java

--- Vehicles.java
+++ Vehicles.java
@@ -1,1 +1,13 @@
+class Vehicles{
+    public void display(){
+        System.out.println("Welcome");
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
+class Car{
+}
 
+class Bike{
+}
